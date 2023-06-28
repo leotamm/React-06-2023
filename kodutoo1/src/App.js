ed89c4cf10c81ca275909a20428c4289a970b0e8
leotamm <73937888+leotamm@users.noreply.github.com>
2023-06-28 12:54:16 +0300

    pilt ja taust tehtud

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <p><button className='nupp'>Vajuta</button></p>
       <p><i className='tekst1'>Sinine tekst</i></p>
       <p><b className='tekst2'>Roheline tekst</b></p>
+      <img className='pilt' src='https://www.testim.io/wp-content/uploads/2021/07/main_image@2x.jpg' alt='JavaScript coder cartoon'/>
     </div>
   );
 }
