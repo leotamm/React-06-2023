96fbf19e4ce5d17aefdda4443148289539a956c7
leotamm <73937888+leotamm@users.noreply.github.com>
2023-06-29 13:47:15 +0300

    pildid paigas

---
+++ portfoolio/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className='main-picture' src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgzMTM5MDM2OTUx/dymaxion-1933.jpg' alt='Dynamaxion 1933' />
+      <div className='rectangle'></div>
+
+      <div className='navigation-pictures'>
+        <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgzMTM5Mjk5MDk1/oldsmobile-golden-rocket-1956.jpg' alt='Oldsmobile Golden Rocket 1956' />
+        <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgyODcwNDA0ODg3/cadillac-cyclone-1959.jpg' alt='Cadillac Cyclone 1959' />
+        <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgyODcwODYzNjM5/ford-gyron-1961.jpg' alt='Ford Gyron 1961' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
