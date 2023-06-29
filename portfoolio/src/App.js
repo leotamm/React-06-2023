31901b91c5f8297618c0c7a3e5e6a80a9059169b
leotamm <73937888+leotamm@users.noreply.github.com>
2023-06-29 14:01:36 +0300

    routes valmis

--- portfoolio/src/App.js
+++ portfoolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -7,10 +8,22 @@ function App() {
       <div className='rectangle'></div>
 
       <div className='navigation-pictures'>
-        <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgzMTM5Mjk5MDk1/oldsmobile-golden-rocket-1956.jpg' alt='Oldsmobile Golden Rocket 1956' />
-        <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgyODcwNDA0ODg3/cadillac-cyclone-1959.jpg' alt='Cadillac Cyclone 1959' />
-        <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgyODcwODYzNjM5/ford-gyron-1961.jpg' alt='Ford Gyron 1961' />
+        <Link to='retro-cars'>
+          <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgzMTM5Mjk5MDk1/oldsmobile-golden-rocket-1956.jpg' alt='Oldsmobile Golden Rocket 1956' />
+        </Link>
+        <Link to='car-collection'>
+          <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgyODcwNDA0ODg3/cadillac-cyclone-1959.jpg' alt='Cadillac Cyclone 1959' />
+        </Link>
+        <Link to='convetions'>
+          <img src='https://www.thestreet.com/.image/ar_3:2%2Cc_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_600/MTY4NTE3MjgyODcwODYzNjM5/ford-gyron-1961.jpg' alt='Ford Gyron 1961' />
+        </Link>
       </div>
+
+      <Routes>
+        <Route path='retro-cars' element={<div>Retro cars</div>} />
+        <Route path='car-collection' element={<div>Car collection</div>} />
+        <Route path='conventions' element={<div>Conventions</div>} />
+      </Routes>
     </div>
   );
 }
