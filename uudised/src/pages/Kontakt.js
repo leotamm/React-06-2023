c0e96654307f9e35b6938a83c148ea6f572a38e5
leotamm <73937888+leotamm@users.noreply.github.com>
2023-06-30 15:41:15 +0300

    telefonid päringu peale

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Kontakt() {
+
+  const [n2itaAlfaTel, muudaN2itaAlfaTel] = useState(false);
+  const [n2itaBeetaTel, muudaN2itaBeetaTel] = useState(false);
+  const [n2itaDeltaTel, muudaN2itaDeltaTel] = useState(false);
+
   return (
     <div>
-        <h1>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</h1>
-        <h2>Võta meiega ühendust:</h2>
-        <br/>
-        <div>Keskus Alfa</div>
-        <div>Telefon: +372 555 6666</div>
-        <div>Paide</div>
-        <br/>
-        <div>Keskus Beeta</div>
-        <div>Telefon: +372 555 7777</div>
-        <div>Haapsalu</div>
-        <br/>
-        <div>Keskus Delta</div>
-        <div>Telefon: +372 555 8888</div>
-        <div>Kuressaare</div>
+      <h1>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</h1>
+      <h2>Võta meiega ühendust:</h2>
+      <br />
+      <div onClick={()=>muudaN2itaAlfaTel(!n2itaAlfaTel)}>Keskus Alfa</div>
+      {n2itaAlfaTel && <div>Telefon: +372 555 6666</div>}
+      <div>Paide</div>
+      <br />
+      <div onClick={()=>muudaN2itaBeetaTel(!n2itaBeetaTel)}>Keskus Beeta</div>
+      {n2itaBeetaTel && <div>Telefon: +372 555 7777</div>}
+      <div>Haapsalu</div>
+      <br />
+      <div onClick={()=>muudaN2itaDeltaTel(!n2itaDeltaTel)}>Keskus Delta</div>
+      {n2itaDeltaTel && <div>Telefon: +372 555 8888</div>}
+      <div>Kuressaare</div>
     </div>
   )
 }
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Meist() {
+
+  const [telefoniNr, annaTelefoniNr] = useState("");
+
   return (
     <div>
       <h1>See on meist leht, nähtav localhost:3000/meist aadressil</h1>
       <h2>Töötajad:</h2>
-        <br/>
-        <div>Mari Maasikas</div>
-        <div>Telefon: +372 555 5555</div>
-        <div>Tegevjuht</div>
-        <br/>
-        <div>Tarmo Tikker</div>
-        <div>Telefon: +372 555 6666</div>
-        <div>Müük</div>
-        <br/>
-        <div>Vambola Vaarikas</div>
-        <div>Telefon: +372 555 7777</div>
-        <div>Tootmine</div>
-        <br/>
-        <div>Villu Virsik</div>
-        <div>Telefon: +372 555 8888</div>
-        <div>Ladu</div>
-        <br/>
+      <br />
+      <div>Mari Maasikas</div>
+      <div>Tegevjuht</div>
+      <button onClick={() => annaTelefoniNr("+372 555 5555")}>Võta ühendust</button>
+      <br /><br />
+      <div>Tarmo Tikker</div>
+      <div>Müük</div>
+      <button onClick={() => annaTelefoniNr("+372 555 6666")}>Võta ühendust</button>
+      <br /><br />
+      <div>Vambola Vaarikas</div>
+      <div>Tootmine</div>
+      <button onClick={() => annaTelefoniNr("+372 555 7777")}>Võta ühendust</button>
+      <br /><br />
+      <div>Villu Virsik</div>
+      <div>Ladu</div>
+      <button onClick={() => annaTelefoniNr("+372 555 8888")}>Võta ühendust</button>
+      <br /><br />
+      {telefoniNr !== "" && <div className='kontakt-tekst'>Tema kontakt: {telefoniNr}</div>}
     </div>
   )
 }
