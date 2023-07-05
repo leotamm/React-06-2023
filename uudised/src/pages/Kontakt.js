bc1bdd4c3bfd812915366fb801e6e9f223da0392
leotamm <73937888+leotamm@users.noreply.github.com>
2023-07-05 12:24:52 +0300

    conditional css and firebase

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -11,17 +11,17 @@ function Kontakt() {
       <h1>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</h1>
       <h2>Võta meiega ühendust:</h2>
       <br />
-      <div onClick={()=>muudaN2itaAlfaTel(!n2itaAlfaTel)}>Keskus Alfa</div>
-      {n2itaAlfaTel && <div>Telefon: +372 555 6666</div>}
-      <div>Paide</div>
+      <div className={n2itaAlfaTel === true ? 'valitud' : undefined} onClick={() => muudaN2itaAlfaTel(!n2itaAlfaTel)}>Keskus Alfa</div>
+      {n2itaAlfaTel === true && <div className='valitud'>Telefon: +372 555 6666</div>}
+      <div className={n2itaAlfaTel ? 'valitud' : undefined}>Paide</div>
       <br />
-      <div onClick={()=>muudaN2itaBeetaTel(!n2itaBeetaTel)}>Keskus Beeta</div>
-      {n2itaBeetaTel && <div>Telefon: +372 555 7777</div>}
-      <div>Haapsalu</div>
+      <div className={n2itaBeetaTel === true ? 'valitud' : undefined} onClick={() => muudaN2itaBeetaTel(!n2itaBeetaTel)}>Keskus Beeta</div>
+      {n2itaBeetaTel === true && <div className='valitud'>Telefon: +372 555 7777</div>}
+      <div className={n2itaBeetaTel ? 'valitud' : undefined}>Haapsalu</div>
       <br />
-      <div onClick={()=>muudaN2itaDeltaTel(!n2itaDeltaTel)}>Keskus Delta</div>
-      {n2itaDeltaTel && <div>Telefon: +372 555 8888</div>}
-      <div>Kuressaare</div>
+      <div className={n2itaDeltaTel === true ? 'valitud' : undefined} onClick={() => muudaN2itaDeltaTel(!n2itaDeltaTel)}>Keskus Delta</div>
+      {n2itaDeltaTel === true && <div className='valitud'>Telefon: +372 555 8888</div>}
+      <div className={n2itaDeltaTel ? 'valitud' : undefined}>Kuressaare</div>
     </div>
   )
 }
