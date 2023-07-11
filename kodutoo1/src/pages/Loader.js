f95d5756e0d9d48e2f90a35211c33f69b7050847
leotamm <73937888+leotamm@users.noreply.github.com>
2023-07-11 16:27:49 +0300

    useState teemad seadete all

--- kodutoo1/src/pages/Loader.js
+++ kodutoo1/src/pages/Loader.js
@@ -6,7 +6,7 @@ const [kasLaeb, uuendaLaadimist] = useState(true);
 
   return (
     <div>
-        { kasLaeb === true && <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>}
+        { kasLaeb === true && <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>}
         <button onClick={() => uuendaLaadimist(false)}>Lõpeta laadimine</button>
     </div>
   )
--- kodutoo1/src/pages/Seaded.js
+++ kodutoo1/src/pages/Seaded.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Seaded() {
+
+  const [kujundus, muudaKujundus] = useState(localStorage.getItem('veebilehe_kujundus'));
+
+  const paneTumeTeema = () => {
+    localStorage.setItem('veebilehe_kujundus', 'dark_mode');
+    muudaKujundus('dark_mode');
+  }
+
+  const paneHeleTeema = () => {
+    localStorage.setItem('veebilehe_kujundus', 'light_mode');
+     muudaKujundus('light_mode');
+  }
+  const paneNeutraalneTeema = () => {
+    localStorage.setItem('veebilehe_kujundus', 'neutral_mode');
+    muudaKujundus('neutral_mode');
+  }
+
   return (
     <div>
-        <div>See on seadete leht</div>
+      <div>See on seadete leht</div>
+      { kujundus!=='dark_mode' && <div><button onClick={paneTumeTeema}>Pane tume teema</button></div>}
+      { kujundus!=='light_mode' && <div><button onClick={paneHeleTeema}>Pane hele teema</button></div>}
+      { kujundus!=='neutral_mode' && <div><button onClick={paneNeutraalneTeema}>Pane neutraalne teema</button></div>}
     </div>
   )
 }
