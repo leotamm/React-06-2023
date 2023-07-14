464411828787c9e77a3a07745dc7a9402993531a
leotamm <73937888+leotamm@users.noreply.github.com>
2023-07-14 15:07:40 +0300

    boilerplate puhas

---
+++ tagasiside/src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <button>Avalehele</button>
+      <button>Tagasiside lehele</button><br />
+
+
+      <Routes>
+        <Route path="/" element='Tere'/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
