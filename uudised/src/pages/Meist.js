f79bfe1bf38a49b1e0987cec67e4d632e7effdaa
leotamm <73937888+leotamm@users.noreply.github.com>
2023-07-19 12:41:22 +0300

    kontaktide lugemine jsonist

--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
+import nimedFailist from '../data/tootajad.json'
 
 function Meist() {
 
-  const [telefoniNr, annaTelefoniNr] = useState("");
+  // const [telefoniNr, annaTelefoniNr] = useState("");
+
+  const tootajad = nimedFailist;
+  const [kontakt, n2itaKontakt] = useState();
 
   return (
     <div className='hele-tekst'>
       <h1>See on meist leht, nähtav localhost:3000/meist aadressil</h1>
       <h2>Töötajad:</h2>
-      <br />
-      <div>Mari Maasikas</div>
+      <br />  
+      {tootajad.map(tootaja =>
+          <div>
+            <div>{tootaja.Nimi}</div>
+            <div>{tootaja.Ala}</div>
+              <button onClick={() => { n2itaKontakt(tootaja.Telefon) }}>Näita telefoni</button>
+            <br /><br />
+          </div>
+      )}      
+      {/* <div>Mari Maasikas</div>
       <div>Tegevjuht</div>
       <button onClick={() => annaTelefoniNr("+372 555 5555")}>Võta ühendust</button>
       <br /><br />
@@ -24,8 +36,8 @@ function Meist() {
       <div>Villu Virsik</div>
       <div>Ladu</div>
       <button onClick={() => annaTelefoniNr("+372 555 8888")}>Võta ühendust</button>
-      <br /><br />
-      {telefoniNr !== "" && <div className='kontakt-tekst'>Tema kontakt: {telefoniNr}</div>}
+      <br /><br /> */}
+      {kontakt !== "" && <div className='hele-tekst'>Tema kontakt: {kontakt}</div>}
     </div>
   )
 }
