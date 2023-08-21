f516d36bc658b0c3ee4ceba25733e1212e67f613
leotamm <73937888+leotamm@users.noreply.github.com>
2023-08-21 16:07:11 +0300

    esmane commit

--- twn-test/src/App.js
+++ twn-test/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Intro from './pages/Article';
+import Article from './pages/Article';
+import List from './pages/Intro';
+import Life from './pages/Life';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar/>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/intro' element={<Intro />} />
+        <Route path='/article' element={<Article />} />
+        <Route path='/list' element={<List />} />
+        <Route path='/life' element={<Life />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ twn-test/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+        
+        <h1>Trinidad Wisemann</h1>
+        <h2>SPA proovitöö</h2>
+    </div>
+  )
+}
+
+export default HomePage
